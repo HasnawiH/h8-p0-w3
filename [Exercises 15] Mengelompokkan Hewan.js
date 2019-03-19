6133de312e48f689cc4c1260a587bf36e860c5fb
awinkh <hasnawihaeba@gmail.com>
2019-03-20 01:31:33 +0700

    menambahkan tugas 15

--- [Exercises 15] Mengelompokkan Hewan.js
+++ [Exercises 15] Mengelompokkan Hewan.js
@@ -1,45 +1,39 @@
 function groupAnimals(animals) {
 
- //buat group animals dengan huruf awal
- var animalsGroups = [];
- for (var i=0; i< animals.length; i++){
-   animalsGroups.push([]); 
-   var indeks = 0;  
-   for (var j=0; j< animals.length; j++) {  
-    if (animals[i][0] === animals[j][0]) { 
-    animalsGroups[i][indeks] = animals[j]; 
-    indeks ++;
-   }
-  }
- }
- 
- 
- //sort untuk nilai array
- var tampung=0;
- for (var i=0; i< animalsGroups.length; i++){
-   for(var j=0; j< animalsGroups.length-i-1; j++){
-     if(animalsGroups[j] > animalsGroups[j+1]){
-       tampung = animalsGroups[j];
-       animalsGroups[j] = animalsGroups[j+1];
-       animalsGroups[j+1] = tampung;
+  // untuk sorting
+ animals.sort()
+
+ var output = [];
+
+ // inialisasi variabel untuk index output
+ var indexOutput = 0
+
+ // looping untu mengambil nilai array
+ for (var i=0; i < animals.length; i++){
+
+     if ( output.length === 0 ){
+
+         output.push([animals[i]])      
+
+         // untuk mengecek huruf pertama nilai output dengan huruf pertama nilai animals
+     } else if(output[indexOutput][0][0] === animals[i][0]){
+
+         output[indexOutput].push(animals[i])
+
+         // jika kondisi diatas tidak terpenuhi
+     } else {
+
+         output.push([animals[i]])
+
+         indexOutput++
      }
-   }
- }
- 
- 
- //buat modus utk menghilangkan duplicate nilai array
- for (var i=0; i< animalsGroups.length-1; i++){
-   if (animalsGroups[i][0] === animalsGroups[i+1][0]){
-     animalsGroups.splice(i,1);
-   }
  }
 
- return animalsGroups;
-  
+ return output
 }
- 
-// TEST CASES
+
+//TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+//[ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+//[ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
